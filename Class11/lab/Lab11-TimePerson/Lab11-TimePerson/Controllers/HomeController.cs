87c9023c6b51dc4d75a96936d528dbc826656f42
Amanda Iverson <ajohnson764@gmail.com>
2020-03-31 09:31:27 -0700

    intial scafffold completed

---
+++ Class11/lab/Lab11-TimePerson/Lab11-TimePerson/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab11_TimePerson.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Default Home route for our home page
+        /// </summary>
+        /// <returns>Generated View</returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Class11/lab/deploy.md
@@ -0,0 +1,7 @@
+# Azure
+
+Steps to deploy:
+
+1. Sign up for an [Azure Account](https://azure.microsoft.com/en-us/account/) for a free 30 day trial.
+2. After registration, right click on the project you want to deploy
+3. Select "Publish"
