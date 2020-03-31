6b667c38be6b0e7db1f12eb0abd875649e0c6379
Amanda Iverson <ajohnson764@gmail.com>
2020-03-31 10:14:39 -0700

    add "starter" code for lab

--- Class11/lab/Lab11-TimePerson/Lab11-TimePerson/Controllers/HomeController.cs
+++ Class11/lab/Lab11-TimePerson/Lab11-TimePerson/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Lab11_TimePerson.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab11_TimePerson.Controllers
@@ -12,9 +13,26 @@ namespace Lab11_TimePerson.Controllers
         /// Default Home route for our home page
         /// </summary>
         /// <returns>Generated View</returns>
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int begYear, int endYear)
+        {
+            // To redirect to our results action with the "results" of our query
+
+            List<TimePerson> persons = TimePerson.GetPersons(begYear, endYear);
+
+            return null;
+        }
+
+        public IActionResult Results(List<TimePerson> persons)
+        {
+           
+            return View(persons);
         }
     }
 }
