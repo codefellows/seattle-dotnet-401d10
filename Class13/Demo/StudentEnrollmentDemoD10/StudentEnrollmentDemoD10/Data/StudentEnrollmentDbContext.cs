f442f2cfaeb4884f38ac7217027ffe7b89847268
Amanda Iverson <ajohnson764@gmail.com>
2020-04-01 16:14:45 -0700

    lab 12 demo

---
+++ Class13/Demo/StudentEnrollmentDemoD10/StudentEnrollmentDemoD10/Data/StudentEnrollmentDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentEnrollmentDemoD10.Data
+{
+    // Don't forget to add Microsoft.EntityFrameworkCore. no other options.
+    public class StudentEnrollmentDbContext : DbContext
+    {
+        public StudentEnrollmentDbContext(DbContextOptions<StudentEnrollmentDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Class13/Demo/StudentEnrollmentDemoD10/StudentEnrollmentDemoD10/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StudentEnrollmentDemoD10.Data;
+
+namespace StudentEnrollmentDemoD10
+{
+    // To use the Package Manager Console for scripting commands, install the following package in the Package Manager Console:
+    // Install-Package Microsoft.EntityFrameworkCore.Tools
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        // bring in library Microsoft.Extensions.Configuration
+        // Set yourself up for Dependency Injection. 
+        // we want the configuration to be...configurable from the Program class. 
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // it's to hold dependencies. 
+            // middlware.
+            services.AddMvc();
+
+            // be sure to install Install-Package Microsoft.EntityFrameworkCore.SqlServer on Package manager console for the use of SQL Server
+            services.AddDbContext<StudentEnrollmentDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        // connection string: path to your db server where your db lives
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
+            });
+        }
+    }
+}
