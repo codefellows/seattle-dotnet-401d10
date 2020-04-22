943715a65eedf62c69e851d4bb4ae172acf07b7b
Amanda Iverson <ajohnson764@gmail.com>
2020-04-22 12:29:01 -0700

    demo class 28

---
+++ Class28/demo/CMSD10DemoCode/CMSD10DemoCode/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CMSD10DemoCode.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSD10DemoCode.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
