32e05d69e5c82cbce181a35db8f3102bdbfa4833
Amanda Iverson <ajohnson764@gmail.com>
2020-04-27 12:51:53 -0700

    add demo code

---
+++ Class31/demo/CMSD10DemoCode/CMSD10DemoCode/Components/TopPosts.cs
@@ -0,0 +1,34 @@
+﻿using CMSD10DemoCode.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSD10DemoCode.Components
+{
+    // You can have either the Tag, or the View Compnents inherited class
+    // you can also have both to be super explicit. 
+    [ViewComponent]
+    public class TopPosts : ViewComponent
+    {
+        private CMSDbContext _context;
+
+        public TopPosts(CMSDbContext context)
+        {
+            _context = context;
+        }
+
+        // Required/reserved method name is Invoke or InvokeAsync
+
+        public async Task<IViewComponentResult> InvokeAsync(int number)
+        {
+            // make a call to my posts table and get the latest 3 results/
+
+           var posts =  _context.Posts.OrderByDescending(potato => potato.ID)
+                            .Take(number);
+
+            return View(posts);
+        }
+    }
+}
---
+++ Class31/demo/CMSD10DemoCode/CMSD10DemoCode/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMSD10DemoCode.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
